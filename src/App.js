0d118209de8d46ce4f42af2c0d312a6c98d24539
Junior Dugue <jrdugue@gmail.com>
2019-08-21 23:39:47 -0400

    installed styled-components, imported createGlobalStyle from styled components, and added google fonts for my headers and p tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.scss';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: blue;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    color: white;
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
     <div className="App">
-
+      <GlobalStyle/>
+      <h1>Jr's PizzaShop</h1>
     </div>
   );
 }
